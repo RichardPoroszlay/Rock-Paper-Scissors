af315601679e985ca68c6ebf48b1fe67ada07199
Richard Poroszlay <richardporoszlay@gmail.com>
2023-10-14 15:21:11 +0200

    Create a function for player choice and another function for simulating one round of the game

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 function getComputerChoice() {
-    let values = ["Rock", "Paper", "Scissors"];
+    let values = ["ROCK", "PAPER", "SCISSORS"];
     let randomize = Math.floor(Math.random() * values.length);
     
     return values[randomize];
 }
 
-console.log(getComputerChoice());
 
-console.log("Hello, World!");
+function getPlayerChoice() {
+    let playerChoice = prompt("Choose between Rock, Paper and Scissors!");
+    switch(playerChoice.toUpperCase()) {
+        case "ROCK":
+            return "ROCK";
+        case "PAPER":
+            return "PAPER";
+        case "SCISSORS":
+            return "SCISSORS";
+    }
+}
+
+function playRound(playerSelection = getPlayerChoice(), computerSelection = getComputerChoice()) {
+    if(playerSelection === computerSelection) {
+        console.log("DRAW");
+    } else if((playerSelection === "ROCK" && computerSelection === "SCISSORS") 
+    || (playerSelection === "PAPER" && computerSelection === "ROCK") 
+    || (playerSelection === "SCISSORS" && computerSelection === "PAPER")) {
+        console.log("PLAYER WON");
+    } else if((playerSelection === "SCISSORS" && computerSelection === "ROCK") 
+    || (playerSelection === "ROCK" && computerSelection === "PAPER")
+    || (playerSelection === "PAPER" && computerSelection === "SCISSORS")) {
+        console.log("COMPUTER WON");
+    } else {
+        console.log("WRONG INPUT");
+    }
+}
+
+playRound();
+playRound();
+playRound();
+playRound();
+playRound();
