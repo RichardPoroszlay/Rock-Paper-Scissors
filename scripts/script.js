58621047147d5102ae5d41f0e1ae22bee30ea46d
Richard Poroszlay <richardporoszlay@gmail.com>
2023-10-26 15:26:05 +0200

    Finish with the major components of the project

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,22 @@ let computerScore = 0;
 
 
 function getComputerChoice() {
+    const computerIcon = document.querySelector("#computerIcon").querySelector("img");
     let values = ["ROCK", "PAPER", "SCISSORS"];
     let randomize = Math.floor(Math.random() * values.length);
     
+    switch(values[randomize]) {
+        case "ROCK":
+            computerIcon.src = "resources/rock.png";
+            break;
+        case "PAPER":
+            computerIcon.src = "resources/paper.png";
+            break;
+        case "SCISSORS":
+            computerIcon.src = "resources/scissors.png";
+            break;
+    }
+
     return values[randomize];
 }
 
@@ -37,29 +50,29 @@ function checkResult(result) {
 function displayScores() {
     const playerScoreDiv = document.querySelector("#playerScore");
     const computerScoreDiv = document.querySelector("#computerScore");
-    playerScoreDiv.textContent = "Player's score: " + playerScore;
-    computerScoreDiv.textContent = "Computer's score: " + computerScore;
+    playerScoreDiv.textContent = "Player: " + playerScore;
+    computerScoreDiv.textContent = "Computer: " + computerScore;
 }
 
 
 function checkScores() {
-    const finalResultDiv = document.querySelector("#finalResult")
-    if(playerScore === 5) {
-        finalResultDiv.textContent = "YOU WON THE MATCH!";
-        playerScore = 0;
-        computerScore = 0;
-    } else if(computerScore === 5) {
-        finalResultDiv.textContent = "YOU LOST THE MATCH!";
-        playerScore = 0;
-        computerScore = 0;
+    const winner = document.querySelector("#winner")
+    if(playerScore >= 5) {
+         modal.style.display = 'block';
+         winner.innerText = "You won the match!";
+    } else if(computerScore >= 5) {
+        modal.style.display = 'block';
+        winner.innerText = "You lost the match!";
     }
 }
 
 
 function playRock() {
     const rockButton = document.querySelector("#rock");
+    const playerIcon = document.querySelector("#playerIcon").querySelector("img");
     rockButton.addEventListener("click", () => {
         let result = playRound("ROCK");
+        playerIcon.src = "resources/rock.png";
         displayResult(result);
         checkResult(result);
         checkScores();
@@ -69,8 +82,10 @@ function playRock() {
 
 function playPaper() {
     const paperButton = document.querySelector("#paper");
+    const playerIcon = document.querySelector("#playerIcon").querySelector("img");
     paperButton.addEventListener("click", () => {
         let result = playRound("PAPER");
+        playerIcon.src = "resources/paper.png";
         displayResult(result);
         checkResult(result);
         checkScores();
@@ -82,6 +97,8 @@ function playScissors() {
     const scissorsButton = document.querySelector("#scissors");
     scissorsButton.addEventListener("click", () => {
         let result = playRound("SCISSORS");
+        const playerIcon = document.querySelector("#playerIcon").querySelector("img");
+        playerIcon.src = "resources/scissors.png";
         displayResult(result);
         checkResult(result);
         checkScores();
@@ -96,7 +113,7 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
         return "DRAW";
     } else if((playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
     (playerSelection === "PAPER" && computerSelection === "ROCK") || 
-    (playerSelection === "SCISSORS" && computerSelection === "PAPER")) {;
+    (playerSelection === "SCISSORS" && computerSelection === "PAPER")) {
         return "PLAYER WON";
     } else if((playerSelection === "SCISSORS" && computerSelection === "ROCK")  ||
     (playerSelection === "ROCK" && computerSelection === "PAPER") ||
@@ -108,10 +125,42 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
 }
 
 
+function operateOnModal() {
+    const modal = document.getElementById('modal');
+    const newGameBtn = document.getElementById('newGameBtn');
+    const closeBtn = document.querySelector('.close');
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    newGameBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+        resetGame();
+    });
+    
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+}
+
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    displayScores();
+    resultDiv.textContent = "Choose your weapon";
+}
+
+
+
 function game() {
     playRock();
     playPaper();
     playScissors();
+    operateOnModal();
 }
 
 
